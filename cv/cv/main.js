e9b6ef0fdb80ece3a899787b51f2768e948314c5
Joren <63969493+joren961@users.noreply.github.com>
2021-07-05 02:18:32 +0200

    added space

--- cv/cv/main.js
+++ cv/cv/main.js
@@ -1,75 +1,77 @@
 import './style.css'
 import * as THREE from 'three';
+import {GridHelper} from 'three';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {WEBGL} from "three/examples/jsm/WebGL";
 
-class Background {
-    constructor() {
-        this.figures = {};
-        this.lights = {};
-        this.controls = null;
-        this.scene = null;
-        this.camera = null;
-        this.renderer = null;
-        this.createBackground();
-        requestAnimationFrame(this.animate);
-        this.animate();
-    }
+if (!WEBGL.isWebGLAvailable()) {
+    let label = document.createElement('label');
+    label.innerText = WEBGL.getWebGLErrorMessage();
+    document.querySelector('#background').append(label);
+} else {
+    //create scene/camera/renderer
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#background'),
+    })
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.setZ(30);
+    scene.background = new THREE.TextureLoader().load('spacev2.png');
 
-    createBackground() {
-        if (!WEBGL.isWebGLAvailable()) {
-            let label = document.createElement('label');
-            label.innerText = WEBGL.getWebGLErrorMessage();
-            document.querySelector('#background').append(label);
-        }
-        this.createScene();
-        this.createFigure();
-        this.createLight();
+    //create scene elements
 
-        for (const [key, value] of Object.entries(this.figures)) {
-            this.scene.add(value);
-        }
-        for (const [key, value] of Object.entries(this.lights)) {
-            this.scene.add(value);
-        }
-    }
-
-    createScene() {
-        let scene = new THREE.Scene();
-
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-       this.renderer = new THREE.WebGLRenderer({
-            canvas: document.querySelector('#background'),
+    const earthTexture = new THREE.TextureLoader().load('earth.jpg');
+    const normalTexture = new THREE.TextureLoader().load('earth_normalmap.png');
+    const earth = new THREE.Mesh(
+        new THREE.SphereGeometry(6,32,32),
+        new THREE.MeshStandardMaterial({
+            map: earthTexture,
+            normalTexture:normalTexture,
         })
+    );
 
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.camera.position.setZ(30);
+    const controls = new OrbitControls(camera, renderer.domElement);
 
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.scene = scene;
-        this.renderer.render(this.scene, this.camera);
+    const light = new THREE.AmbientLight(0xffffff);
 
+    const gridhelper = new GridHelper(200, 50);
+
+    //add to scene
+    scene.add(light, earth);
+    renderer.render(scene, camera);
+
+    //animate figure1
+    function animate() {
+        requestAnimationFrame(animate);
+        earth.rotation.x += 0.0002;
+        earth.rotation.y += 0.005;
+        earth.rotation.z += 0.0002;
+        controls.update();
+        renderer.render(scene, camera);
+    }
+    animate();
+
+
+    function generatestars() {
+        const geom = new THREE.SphereGeometry(0.14, 24, 24);
+        const mat = new THREE.MeshStandardMaterial({color: 0xffffff});
+        const star = new THREE.Mesh(geom, mat);
+
+        const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+        star.position.set(x, y, z);
+        scene.add(star);
     }
 
-    createFigure() {
-        const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-        const material = new THREE.MeshStandardMaterial({color: 0xFF6347, wireframe: true});
-        this.figures.figure1 = new THREE.Mesh(geometry, material);
-    }
+    Array(200).fill().forEach(generatestars);
 
-    createLight() {
-        this.lights.light1 = new THREE.AmbientLight(0xffffff);
-    }
-
-    animate() {
-        this.figures.figure1.rotation.x += 0.01;
-        this.figures.figure1.rotation.y += 0.04;
-        this.figures.figure1.rotation.z += 0.1;
-        this.controls.update();
-        this.renderer.render(this.scene, this.camera);
-    }
+    // function movecamera() {
+    //     const top = document.body.getBoundingClientRect().top;
+    // }
+    //
+    // document.body.onscroll = movecamera;
 }
 
-new Background();
+
+
