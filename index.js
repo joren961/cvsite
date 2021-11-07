30b2bdaa491a3cd45fd81f16a3bc62f34399472f
Joren <63969493+joren961@users.noreply.github.com>
2021-11-07 20:47:14 +0100

    updates

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('get', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+}
+);
+
+app.listen(() => {
+    console.log('live on http://localhost:3000/');
+});
