60c949938a34bc6c203d6ddd3febf0f788244e69
Joren <63969493+joren961@users.noreply.github.com>
2021-11-07 21:30:26 +0100

    adad

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+server.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
 let PORT = process.env.PORT || '3000'
 app.set('port', PORT);
 app.use(express.static("public"));
