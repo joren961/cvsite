df2b30396b816601551fe8cf3e58e44a18d4b35d
Joren <63969493+joren961@users.noreply.github.com>
2021-11-07 20:48:22 +0100

    ada

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('get', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
 }
 );
