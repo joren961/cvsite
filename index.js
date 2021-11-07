46e212899a473d3f09b68b6d7183e4430e22fcb5
Joren <63969493+joren961@users.noreply.github.com>
2021-11-07 20:53:51 +0100

    adad

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
-}
-);
+});
+let port_number = server.listen(process.env.PORT || 3000);
 
-app.listen(() => {
-    console.log('live on http://localhost:3000/');
+app.listen(port_number,() => {
+    console.log('server live');
 });
