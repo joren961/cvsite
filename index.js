7ba77d6b155d884c8034e8236187825ea49a4f92
Joren <63969493+joren961@users.noreply.github.com>
2021-11-07 21:21:04 +0100

    updates

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
-});
 let PORT = process.env.PORT || '3000'
 app.set('port', PORT);
+app.use(express.static("public"));
 
 app.listen(PORT,() => {
     console.log('server live');
