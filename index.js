00493f6f437dcc0fab74aca9dd191a9dc4ce03a0
Joren <63969493+joren961@users.noreply.github.com>
2021-11-08 16:21:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ let PORT = process.env.PORT || '3000'
 app.set('port', PORT);
 app.use('/public', express.static(__dirname + '/public'));
 
-server.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-    res.sendFile(__dirname + '/public/style.css');
-    res.sendFile(__dirname + '/public/main.js');
-})
-
 app.listen(PORT,() => {
     console.log('server live');
 });
